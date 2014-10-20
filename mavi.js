eb550fa82c190873a415f3a205982fec4aa5f80c
Eray ARSLAN <relfishere@gmail.com>
2014-10-20 14:19:48 +0300

    dolly added

--- mavi.js
+++ mavi.js
@@ -31,6 +31,47 @@
         return dummy;
     };
 
+    // deep object cloning with pure javascript (jquery "extend" function based)
+    //
+    // full documantation : http://api.jquery.com/jquery.extend/
+    //
+    mavi.dolly = function() {
+        var options, name, src, copy, copyIsArray, clone, target = arguments[0] || {}, i = 1, length = arguments.length, deep = false;
+        if (typeof target === "boolean") {
+            deep = target; target = arguments[ i ] || {}; i++;
+        }
+        if (typeof target !== "object" && !(Object.prototype.toString.call( target ) === '[object Function]')) {
+            target = {};
+        }
+        if (i === length) {
+            target = this; i--;
+        }
+        for (; i < length; i++) {
+            if ((options = arguments[ i ]) != null) {
+                for (name in options) {
+                    src = target[name];
+                    copy = options[name];
+                    if (target === copy) {
+                        continue;
+                    }
+                    if (deep && copy && ( (copy.toString()==="[object Object]") ||
+                        (copyIsArray = (Object.prototype.toString.call( copy ) === '[object Array]')) )) {
+                        if (copyIsArray) {
+                            copyIsArray = false;
+                            clone = src && (Object.prototype.toString.call( src ) === '[object Array]') ? src : [];
+
+                        } else {
+                            clone = src && (src.toString()==="[object Object]") ? src : {};
+                        }
+                        target[name] = this.dolly( deep, clone, copy );
+                    } else if (copy !== undefined) {
+                        target[name] = copy;
+                    }
+                }
+            }
+        } return target;
+    }
+
     return mavi;
 
 }));
--- test/tests.js
+++ test/tests.js
@@ -5,4 +5,17 @@
         strictEqual(mavi.getDummy(), "dummy");
     });
 
+    // "dolly" test
+    test("dolly", 5, function() {
+        var clonedObject = mavi.dolly(true, mavi.dolly(true, {}, mavi), {"x": "x"});
+        //
+        strictEqual(clonedObject.x, "x");
+        notStrictEqual(clonedObject.x, undefined);
+        //
+        strictEqual(mavi.x, undefined);
+        notStrictEqual(mavi.x, "x");
+        //
+        strictEqual(mavi.getDummy(), clonedObject.getDummy());
+    });
+
 })();
